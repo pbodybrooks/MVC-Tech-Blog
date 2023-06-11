f316b8a68b601554c326080ee0590c4f90950583
Parker <parkerlamarbrook@gmail.com>
2023-06-11 14:26:43 -0600

    created Comment.js model

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },       
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'name',
+            },
+        },
+    },
+    {
+        sequelize,
+        // TODO: confirm timestamps interaction with DataTypes.DATE/.NOW
+        // timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
