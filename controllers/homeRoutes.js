34d65b1d1c3edb330546c3f1e567d398238e29dc
Parker <parkerlamarbrook@gmail.com>
2023-06-11 18:07:54 -0600

    continued building out handlebars. allPosts, singlePost, login, logout, etc.)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,25 +2,41 @@ const router = require('express').Router();
 const { Comment, Post, User } = require('../models');
 
 // get all posts to be shown on homepage
+// router.get('/', async (req, res) => {
+//     try {
+//         const postData = await Post.findAll({
+//             include: [
+//                 {
+//                     model: User,
+//                     attributes: ['name'],
+//                 },
+//             ],
+//         });
+//         const posts = postData.map((post) => post.get({ plain: true }));
+//         res.render('homepage', {
+//             posts,
+//             loggedIn: req.session.loggedIn,
+//         });
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 router.get('/', async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ],
+    Post.findAll({
+        include: [User],
+    })
+    .then((allPosts) => {
+        const posts = allPosts.map((post) => post.get({ plain: true }));
+
+        res.render("allPosts",  { 
+            posts, 
+            loggedIn: req.session.loggedIn 
         });
-        const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('homepage', {
-            posts,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        console.log(err);
+    })
+    .catch((err) => {
         res.status(500).json(err);
-    }
+    });
 });
 
 // get single post by id
@@ -36,7 +52,7 @@ router.get('/post/:id', async (req, res) => {
             ],
         });
         const post = postData.get({ plain: true });
-        res.render('post', {
+        res.render('singlePost', {
             ...post,
             loggedIn: req.session.loggedIn,
         });
@@ -44,6 +60,16 @@ router.get('/post/:id', async (req, res) => {
         console.log(err);
         res.status(500).json(err);
     }
+});
+
+// register route, if logged in redirect to homepage/dashboard
+router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
 });
 
 // login page
